ba2e95fecab0c38ff662ab0e0b07950cefac673d
Samkipz <ndiemasam@gmail.com>
2024-10-04 14:17:44 +0300

    Adjusted pages heights. Fixed mobile screen sheet on open and close1

--- app/ui/hero/Hero.jsx
+++ app/ui/hero/Hero.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function HeroSection() {
   return (
     <div id="hero"
-      className='w-full h-ful py-24 px-4 md:px-12 sm:px-6 lg:px-8 relative flex flex-col items-start justify-center'
+      className='scroll-mt-20 scroll-smooth w-full h-ful py-24 px-4 md:px-12 sm:px-6 lg:px-8 relative flex flex-col items-start justify-center'
       style={{ 
         backgroundImage: 'url(/hero1.jpeg)', 
         backgroundSize: 'cover', 
