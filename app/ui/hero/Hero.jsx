0d868e3b0c99cfeb26d9520f8f19a3984aeb6799
RISHARD. M . BAKUNDA <133122984+Rishardmunene@users.noreply.github.com>
2024-10-19 16:00:43 +0300

    Request a quote color blue-700

--- app/ui/hero/Hero.jsx
+++ app/ui/hero/Hero.jsx
@@ -250,7 +250,7 @@ export default function HeroSection() {
               </button>
               <button 
                 type="submit"
-                className="px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md hover:bg-blue-700"
+                className="px-4 py-2 text-sm font-medium text-white bg-blue-700 border border-transparent rounded-md hover:bg-blue-700"
               >
                 Submit Request
               </button>
