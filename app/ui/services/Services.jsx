7044836d4823cb0bdb74af01de1afa3eb6002e0d
Samkipz <ndiemasam@gmail.com>
2024-10-20 10:41:16 +0300

    Redone service and industries section

--- app/ui/services/Services.jsx
+++ app/ui/services/Services.jsx
@@ -1,122 +1,204 @@
+"use client";
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Link from "next/link";
+import Image from "next/image";
 import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-  CardDescription,
-} from "@/components/ui/card";
-import { Separator } from "@/components/ui/separator";
+  ChartNetwork,
+  CloudCog,
+  FileCheck2,
+  MonitorSmartphone,
+  TrainFront,
+  UserCog,
+} from "lucide-react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
-export default function IndustriesAndServices() {
+export default function Services() {
+  const industries = [
+    { image: "/agritech.jpg", title: "Agriculture" },
+    { image: "/health.jpg", title: "Health" },
+    { image: "/Education.jpg", title: "Education" },
+    { image: "/ES.jpg", title: "Enterprise Solutions" },
+    { image: "/fintech.png", title: "FinTech" },
+    { image: "/iot.jpg", title: "Smart Cities & IoT" },
+  ];
+  const settings = {
+    arrows: false,
+    autoplay: true,
+    speed: 3000,
+    autoplaySpeed: 3000,
+    cssEase: "linear",
+    dots: false,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          speed: 2000,
+        },
+      },
+    ],
+  };
+
   return (
     <section
       id="services"
       className="flex flex-col md:justify-center md:items-center xl:justify-start py-2 w-full  scroll-mt-20 scroll-smooth min-h-[90vh] "
     >
-      <div className="container xl:max-w-6xl mx-auto ">
+      <div className="container xl:max-w-6xl mx-auto px-2">
+        {/* Key Industries Section */}
         <h2 className="text-2xl sm:text-3xl md:text-3xl font-bold text-center md:text-left bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-blue-700">
-          Industries and Services
+          What We Do
         </h2>
-        <div className="flex flex-col gap-2 md:flex-row w-full border-2 border-red-500">ttt</div>
+
+        <div className="py-4">
+          <p className="text-md sm:text-lg md:text-xl lg:text-2xl font-bold text-center md:text-left whitespace-nowrap pb-2">
+            Key Industries
+          </p>
+          <Slider {...settings}>
+            {industries.map((industry, index) => (
+              <div key={index} className="px-2">
+                <div className="group relative overflow-hidden rounded-lg shadow-lg">
+                  <Image
+                    src={industry.image}
+                    alt={industry.title}
+                    className="h-48 w-full object-cover transition-transform duration-300 ease-in-out group-hover:scale-105"
+                    width={400}
+                    height={250}
+                    priority
+                  />
+
+                  <div className="absolute inset-0 bg-black bg-opacity-60 opacity-100 flex items-center justify-center p-6">
+                    <div className="text-white text-center">
+                      <h3 className="text-lg font-semibold">
+                        {industry.title}
+                      </h3>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
+        {/* End Key Industries Section */}
+
+        {/* Services Section */}
+        <section className="py-4">
+          <h2 className="text-md sm:text-lg md:text-xl lg:text-2xl font-bold text-center md:text-left whitespace-nowrap pb-2">
+            Our Services
+          </h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            {[
+              {
+                icon: <MonitorSmartphone size={45} />,
+                title: "Web & Mobile Development",
+                description:
+                  "Building modern, responsive web and mobile applications tailored to your business needs.",
+                details:
+                  "At Kodetrix, we offer expertise in web and mobile development, including front-end and back-end development, UI/UX design, and testing.",
+              },
+              {
+                icon: <FileCheck2 size={45} />,
+                title: "IT Consultancy",
+                description:
+                  "Providing expert IT consulting services to optimize your infrastructure and operations.",
+              },
+              {
+                icon: <UserCog size={45} />,
+                title: "IT Support",
+                description:
+                  "Comprehensive IT support to ensure seamless business continuity and technology management.",
+              },
+              {
+                icon: <TrainFront size={45} />,
+                title: "AI & Automation",
+                description:
+                  "Leveraging AI and automation technologies to enhance efficiency and innovation.",
+              },
+              {
+                icon: <CloudCog size={45} />,
+                title: "DevOps & Cloud Computing",
+                description:
+                  "Streamlining your cloud infrastructure and DevOps pipelines for scalable, efficient solutions.",
+              },
+              {
+                icon: <ChartNetwork size={45} />,
+                title: "Data Analytics",
+                description:
+                  "Unlocking actionable insights from data to drive informed business decisions.",
+              },
+            ].map((service, index) => (
+              <Dialog key={index}>
+                <DialogTrigger className="p-4 border rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                  <div className="py-2 px-2 ">
+                    <div className="flex flex-col items-center justify-center mb-4 ">
+                      {service.icon}
+                      <h4 className="mt-2 text-lg leading-normal mb-1 font-semibold">
+                        {service.title}
+                      </h4>
+                    </div>
+
+                    {/* Accordion for description */}
+                    {/* <details className="text-gray-500 dark:text-gray-400">
+                    <summary className="cursor-pointer font-medium">
+                      Learn more
+                    </summary>
+                    <p className="mt-2">{service.description}</p>
+                  </details> */}
+                  </div>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                  <DialogHeader>
+                    <DialogTitle>{service.title}</DialogTitle>
+                    <DialogDescription>{service.description}</DialogDescription>
+                  </DialogHeader>
+                  <div className="flex flex-col items-center justify-center mt-4">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Accusantium delectus accusamus modi voluptas minus,
+                    similique a distinctio dolores quas repudiandae magni
+                    voluptatum iusto animi saepe magnam dolore? Provident,
+                    aspernatur aliquid assumenda, facere unde repudiandae
+                    obcaecati labore dolor in animi, esse tempora impedit optio
+                    inventore id rerum illum accusantium iure nam.
+                  </div>
+                </DialogContent>
+              </Dialog>
+            ))}
+          </div>
+        </section>
+        {/* End Services Section */}
       </div>
     </section>
-    // <section id="industries-services" className="py-12 px-4 lg:px-8">
-    //   {/* Header Section */}
-    //   <div className="text-center mb-10">
-    //     <h2 className="text-3xl font-bold tracking-tight">Industries and Services</h2>
-    //     <p className="text-lg text-muted-foreground mt-2">
-    //       Learn about the industries we serve and the services we offer.
-    //     </p>
-    //   </div>
-
-    //   {/* Services Section */}
-    //   <div className="grid gap-8 lg:grid-cols-2">
-    //     <Card>
-    //       <CardHeader>
-    //         <CardTitle>Comprehensive IT Solutions</CardTitle>
-    //         <CardDescription>
-    //           Delivering solutions tailored to your industry.
-    //         </CardDescription>
-    //       </CardHeader>
-    //       <CardContent>
-    //         <p>
-    //           We provide IT solutions that address current market challenges in Finance, Agriculture, Health, and more.
-    //         </p>
-    //       </CardContent>
-    //     </Card>
-
-    //     <Card>
-    //       <CardHeader>
-    //         <CardTitle>Strategic Industry Focus</CardTitle>
-    //         <CardDescription>
-    //           Expertise in key industry verticals.
-    //         </CardDescription>
-    //       </CardHeader>
-    //       <CardContent>
-    //         <p>
-    //           Our expertise spans various industries such as Finance, Agriculture, Health, and others, ensuring solutions fit their unique needs.
-    //         </p>
-    //       </CardContent>
-    //     </Card>
-    //   </div>
-
-    //   {/* Separator */}
-    //   <Separator className="my-12" />
-
-    //   {/* Industries Section */}
-    //   <div className="grid gap-8 lg:grid-cols-2">
-    //     {/* Finance */}
-    //     <Card>
-    //       <CardHeader>
-    //         <CardTitle>Finance</CardTitle>
-    //         <CardDescription>Innovative IT solutions for the financial industry.</CardDescription>
-    //       </CardHeader>
-    //       <CardContent>
-    //         <p>
-    //           We streamline financial operations with advanced technology, enabling growth and sustainability.
-    //         </p>
-    //       </CardContent>
-    //     </Card>
-
-    //     {/* Agriculture */}
-    //     <Card>
-    //       <CardHeader>
-    //         <CardTitle>Agriculture</CardTitle>
-    //         <CardDescription>Technology solutions for modern agriculture.</CardDescription>
-    //       </CardHeader>
-    //       <CardContent>
-    //         <p>
-    //           We help optimize agricultural processes, increasing efficiency and reducing costs.
-    //         </p>
-    //       </CardContent>
-    //     </Card>
-
-    //     {/* Health */}
-    //     <Card>
-    //       <CardHeader>
-    //         <CardTitle>Health</CardTitle>
-    //         <CardDescription>Improving healthcare systems with technology.</CardDescription>
-    //       </CardHeader>
-    //       <CardContent>
-    //         <p>
-    //           Our healthcare solutions enhance patient care, data management, and operational efficiency.
-    //         </p>
-    //       </CardContent>
-    //     </Card>
-
-    //     {/* Custom Industry */}
-    //     <Card>
-    //       <CardHeader>
-    //         <CardTitle>Custom Solutions</CardTitle>
-    //         <CardDescription>Tailored IT solutions for emerging industries.</CardDescription>
-    //       </CardHeader>
-    //       <CardContent>
-    //         <p>
-    //           We also offer custom IT solutions for industries that need personalized technology support.
-    //         </p>
-    //       </CardContent>
-    //     </Card>
-    //   </div>
-    // </section>
   );
 }
